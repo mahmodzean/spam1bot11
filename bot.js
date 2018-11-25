468ce5d872ec8562c4846b7d0c249bea924cf677
mahmodzean <44239884+mahmodzean@users.noreply.github.com>
2018-11-25 20:01:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("513023117006995471")
+let channel =     client.channels.get("516312325947392020")
 setInterval(function() {
 channel.send(`mmmmmmmmmmmmmmmmmmmmmmmmm mahmod pro xd pro xdxdxdxdxdxdxdxdxd`);
 }, 30)
